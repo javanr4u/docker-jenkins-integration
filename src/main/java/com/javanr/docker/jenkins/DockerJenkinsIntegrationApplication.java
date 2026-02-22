9beb6223a028d92c48749414babc14e2f0fb5eab
Narasimha Reddy <javanr4u@gmail.com>
2026-02-22 16:44:00 +0530

    second commit

--- src/main/java/com/javanr/docker/jenkins/DockerJenkinsIntegrationApplication.java
+++ src/main/java/com/javanr/docker/jenkins/DockerJenkinsIntegrationApplication.java
@@ -1,12 +1,16 @@
 package com.javanr.docker.jenkins;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DockerJenkinsIntegrationApplication {
 
+    static Logger logger = LoggerFactory.getLogger(DockerJenkinsIntegrationApplication.class);
     public static void main(String[] args) {
+        logger.info("Started docker-jenkins-integration from jenkins.....");
         SpringApplication.run(DockerJenkinsIntegrationApplication.class, args);
     }
 
